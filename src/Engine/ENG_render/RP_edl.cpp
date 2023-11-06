b0586d1da395c0944840e5398e44365ba5f743fb
nsviel <nathan.sanchizviel@pm.me>
2023-11-06 18:38:00 +0100

    up_vulkan

--- src/Engine/ENG_render/RP_edl.cpp
+++ src/Engine/ENG_render/RP_edl.cpp
@@ -83,7 +83,8 @@ void RP_edl::draw_edl(Struct_renderpass* renderpass){
 void RP_edl::update_descriptor(Struct_renderpass* renderpass){
   //---------------------------
 
-  Struct_frame* frame_scene = struct_vulkan->vec_renderpass[0]->get_renderpass_frame();
+  //Struct_frame* frame_scene = struct_vulkan->vec_renderpass[0]->get_renderpass_frame();
+  Struct_frame* frame_scene = struct_vulkan->swapchain.get_frame_rendering();
   for(int i=0; i<renderpass->vec_pipeline.size(); i++){
     Struct_pipeline* pipeline = renderpass->vec_pipeline[i];
     vk_descriptor->update_descriptor_sampler(&pipeline->binding, &frame_scene->color);
