f0a19d40980d630a14ef2165694dff70a87f9e4f
nsviel <nathan.sanchizviel@pm.me>
2023-11-08 19:34:39 +0100

    up_vulkan

--- src/Engine/ENG_shader/Canvas/CAN_shader.cpp
+++ src/Engine/ENG_shader/Canvas/CAN_shader.cpp
@@ -22,8 +22,8 @@ void CAN_shader::init_shader(){
   Shader_info* shader_info = new Shader_info();
   shader_info->title = "Canvas";
   shader_info->folder = "Canvas";
-  shader_info->path_vs = "shader_canvas_empty_vs";
-  shader_info->path_fs = "shader_canvas_empty_fs";
+  shader_info->path_vs = "shader_empty_vs";
+  shader_info->path_fs = "shader_empty_fs";
   shader_info->compile_shader = true;
   shader_info->with_depth_test = false;
   this->vec_shader_info.push_back(shader_info);
