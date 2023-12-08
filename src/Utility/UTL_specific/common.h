37f345b12c73481130c7d50c628317b248a536f0
nsviel <nathan.sanchizviel@pm.me>
2023-12-08 10:51:31 +0100

    up

--- src/Utility/UTL_specific/common.h
+++ src/Utility/UTL_specific/common.h
@@ -42,10 +42,6 @@ using namespace glm;
 #include <UTL_function/fct_timer.h>  //For time profilling
 
 //Data
-#include <UTL_base/Struct_object.h>
-#include <UTL_base/Struct_set.h>
-#include <UTL_base/Struct_file.h>
-#include <UTL_base/Struct_glyph.h>
 #include <UTL_base/data_nsp.h>
 
 //GUI
