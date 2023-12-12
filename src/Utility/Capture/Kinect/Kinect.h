6fe267b26afde1a5846c1cbe4a81bcabbe394de5
nsviel <nathan.sanchizviel@pm.me>
2023-12-12 10:18:54 +0100

    up

--- src/Utility/Capture/Kinect/Kinect.h
+++ src/Utility/Capture/Kinect/Kinect.h
@@ -2,15 +2,13 @@
 
 #include <Utility/Capture/Kinect/Structure/Namespace.h>
 #include <Utility/Capture/Kinect/Thread/Namespace.h>
-#include <Utility/Capture/Kinect/Device/K4A_device.h>
-#include <Utility/Specific/common.h>
 #include <Utility/Capture/Kinect/Namespace.h>
 #include <Utility/Capture/Kinect/Device/K4A_swarm.h>
+#include <Utility/Capture/Kinect/Device/K4A_device.h>
+#include <Utility/Specific/common.h>
 
 #include <k4a/k4a.h>
 #include <k4a/k4a.hpp>
-
-class K4A_swarm;
 
 
 namespace util::kinect{
