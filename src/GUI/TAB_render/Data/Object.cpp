d785964b95c5cfee6169130316ae77a44a3af064
nsviel <nathan.sanchizviel@pm.me>
2023-12-14 16:11:26 +0100

    bug_object_panel_ok

--- src/GUI/TAB_render/Data/Object.cpp
+++ src/GUI/TAB_render/Data/Object.cpp
@@ -29,7 +29,7 @@ void Object::run_panel(){
 
   if(*panel_show){
     ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.1, 0.1, 0.1, 1));
-    if(ImGui::Begin(panel_name.c_str(), panel_show, ImGuiWindowFlags_AlwaysAutoResize) == 1){
+    if(ImGui::Begin(panel_name.c_str(), panel_show, ImGuiWindowFlags_AlwaysAutoResize)){
 
       this->design_panel();
 
@@ -58,82 +58,78 @@ void Object::object_info(eng::structure::Object* object){
   //---------------------------
 }
 void Object::object_parameter(eng::structure::Object* object){
-  ImGui::Columns(2);
+
   //---------------------------
 
-  //Visibility
-  ImGui::Text("Visibility");
-  ImGui::NextColumn();
-  ImGui::Checkbox("##4555", &object->is_visible);
-  if(object->is_suppressible){
-    ImGui::SameLine();
-    if(ImGui::Button(ICON_FA_TRASH "##4567")){
-      eng_scene->delete_scene_object(object);
-      this->object_selected = nullptr;
+  if(ImGui::BeginTable("object##table", 2, ImGuiTableFlags_BordersInnerV)){
+    //Visibility
+    ImGui::TableNextRow(); ImGui::TableNextColumn();
+    ImGui::Text("Visibility"); ImGui::TableNextColumn();
+    ImGui::Checkbox("##4555", &object->is_visible);
+    if(object->is_suppressible){
+      ImGui::SameLine();
+      if(ImGui::Button(ICON_FA_TRASH "##4567")){
+        eng_scene->delete_scene_object(object);
+        this->object_selected = nullptr;
+      }
     }
-  }
-  ImGui::NextColumn();
 
-  //Name
-  ImGui::SetColumnWidth(-1,75);
-  ImGui::Text("Name");
-  ImGui::NextColumn();
-  static char str_n[256];
-  strcpy(str_n, object->name.c_str());
-  if(ImGui::InputText("##name", str_n, IM_ARRAYSIZE(str_n), ImGuiInputTextFlags_EnterReturnsTrue)){
-    object->name = str_n;
-  }
-  ImGui::NextColumn();
+    //Name
+    ImGui::TableNextRow(); ImGui::TableNextColumn();
+    ImGui::Text("Name"); ImGui::TableNextColumn();
+    static char str_n[256];
+    strcpy(str_n, object->name.c_str());
+    if(ImGui::InputText("##name", str_n, IM_ARRAYSIZE(str_n), ImGuiInputTextFlags_EnterReturnsTrue)){
+      object->name = str_n;
+    }
 
-  //Format
-  ImGui::Text("Format ");
-  ImGui::NextColumn();
-  static char str_f[256];
-  strcpy(str_f, object->file_format.c_str());
-  if(ImGui::InputText("##format", str_f, IM_ARRAYSIZE(str_f), ImGuiInputTextFlags_EnterReturnsTrue)){
-    object->file_format = str_f;
-  }
-  ImGui::NextColumn();
+    //Format
+    ImGui::TableNextRow(); ImGui::TableNextColumn();
+    ImGui::Text("Format"); ImGui::TableNextColumn();
+    static char str_f[256];
+    strcpy(str_f, object->file_format.c_str());
+    if(ImGui::InputText("##format", str_f, IM_ARRAYSIZE(str_f), ImGuiInputTextFlags_EnterReturnsTrue)){
+      object->file_format = str_f;
+    }
 
-  //Uniform collection color
-  ImGui::Text("Uniform color");
-  ImGui::NextColumn();
-  ImGuiColorEditFlags flags = ImGuiColorEditFlags_NoInputs;
-  flags |= ImGuiColorEditFlags_AlphaBar;
-  if(ImGui::ColorEdit4("Color", (float*)&object->unicolor, flags)){
-    //colorManager->set_color_new(object, object->unicolor);
-  }
-  ImGui::NextColumn();
+    //Uniform collection color
+    ImGui::TableNextRow(); ImGui::TableNextColumn();
+    ImGui::Text("Color"); ImGui::TableNextColumn();
+    ImGuiColorEditFlags flags = ImGuiColorEditFlags_NoInputs;
+    flags |= ImGuiColorEditFlags_AlphaBar;
+    if(ImGui::ColorEdit4("Color", (float*)&object->unicolor, flags)){
+      //colorManager->set_color_new(object, object->unicolor);
+    }
 
-  //Root pos
-  vec3& root = object->root;
-  ImGui::Text("Root ");
-  ImGui::NextColumn();
-  ImGui::Text("%.2f  %.2f  %.2f", root.x, root.y, root.z);
-  ImGui::SameLine();
-  if(ImGui::Button("R", ImVec2(15,0))){
-    root = vec3(0,0,0);
-  }
-  ImGui::NextColumn();
-
-  //Primitive size
-  if(object->draw_type_name == "point"){
-    //Number of points
-    ImGui::Text("Nb point");
+    //Root pos
+    ImGui::TableNextRow(); ImGui::TableNextColumn();
+    vec3& root = object->root;
+    ImGui::Text("Root"); ImGui::TableNextColumn();
     ImGui::NextColumn();
-    string nb_point = math::thousand_separator(object->nb_point);
-    ImGui::Text("%s", nb_point.c_str());
-    ImGui::NextColumn();
+    ImGui::Text("%.2f  %.2f  %.2f", root.x, root.y, root.z);
+    ImGui::SameLine();
+    if(ImGui::Button("R", ImVec2(15,0))){
+      root = vec3(0,0,0);
+    }
 
-    this->size_point(object);
-  }
-  else if(object->draw_type_name == "line"){
-    this->width_line(object);
+    //Primitive size
+    if(object->draw_type_name == "point"){
+      //Number of points
+      ImGui::TableNextRow(); ImGui::TableNextColumn();
+      ImGui::Text("Nb point"); ImGui::TableNextColumn();
+      string nb_point = math::thousand_separator(object->nb_point);
+      ImGui::Text("%s", nb_point.c_str());
+
+      this->size_point(object);
+    }
+    else if(object->draw_type_name == "line"){
+      this->width_line(object);
+    }
+
+    ImGui::EndTable();
   }
 
   //---------------------------
-  ImGui::Columns(1);
-  //ImGui::Separator();
 }
 
 //Primitive size
@@ -144,8 +140,8 @@ void Object::width_line(eng::structure::Object* object){
   //Column 1
   ImGui::SetNextItemWidth(item_width);
   ImGui::AlignTextToFramePadding();
-  ImGui::Text("Line width ");
-  ImGui::NextColumn();
+  ImGui::TableNextRow(); ImGui::TableNextColumn();
+  ImGui::Text("Line width"); ImGui::TableNextColumn();
 
   //Column 2
   ImGui::PushButtonRepeat(true);
@@ -173,8 +169,8 @@ void Object::size_point(eng::structure::Object* object){
   //Column 1
   ImGui::SetNextItemWidth(item_width);
   ImGui::AlignTextToFramePadding();
-  ImGui::Text("Point size ");
-  ImGui::NextColumn();
+  ImGui::TableNextRow(); ImGui::TableNextColumn();
+  ImGui::Text("Point size"); ImGui::TableNextColumn();
 
   //Column 2
   ImGui::PushButtonRepeat(true);
