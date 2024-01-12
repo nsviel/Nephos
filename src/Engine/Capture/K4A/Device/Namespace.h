0fe4b725c6f6928b490e2ab88ee2b7d38fa465be
nsviel <nathan.sanchizviel@pm.me>
2024-01-12 11:29:50 +0100

    refractoring_k4A

---
+++ src/Engine/Capture/K4A/Device/Namespace.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Engine/Capture/K4A/Device/K4A_device.h>
+#include <Engine/Capture/K4A/Device/K4A_swarm.h>
+
+
+namespace k4a::device{
+  //---------------------------
+
+  class K4A_device;
+  class K4A_swarm;
+
+  //---------------------------
+}
