896d9d42957c6c95fc4ceb5a6593a3fca60fc2a4
nsviel <nathan.sanchizviel@pm.me>
2024-01-17 18:40:13 +0100

    up

--- src/Utility/Base/Entity/Struct_entity.h
+++ src/Utility/Base/Entity/Struct_entity.h
@@ -1,33 +1,3 @@
 #pragma once
 
 #include <Utility/Specific/common.h>
-
-
-namespace entity{
-
-struct Entity
-{
-  //---------------------------
-
-  virtual ~Entity(){}
-  virtual void update_entity(){}
-  virtual void remove_entity(){}
-  virtual void reset_entity(){}
-  virtual void visibility_entity(bool value){}
-  virtual utl::type::Data* get_data(){return nullptr;}
-
-  int UID = -1;
-  bool is_suppressible = true;
-  bool is_permanent = false;
-  bool is_movable = true;
-  bool is_visible = true;
-  std::string name = "";
-  std::string type = "entity";
-  //utl::type::Set* set_parent = nullptr;
-
-  inline bool get_visibility(){return is_visible;}
-
-  //---------------------------
-};
-
-}
