42fb2ba9b6bf249fba56e29dc15cb4c850dd8b95
nsviel <nathan.sanchizviel@pm.me>
2024-01-23 16:50:50 +0100

    up

--- src/GUI/TAB_render/Engine/Namespace.h
+++ src/GUI/TAB_render/Engine/Namespace.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <GUI/TAB_render/Engine/Control.h>
+#include <Engine/Operation/gui/Control.h>
 #include <GUI/TAB_render/Engine/Shader.h>
 #include <GUI/TAB_render/Engine/Profiler.h>
 #include <Engine/Render/gui/Renderer.h>
