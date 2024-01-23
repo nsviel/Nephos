64961bb6e352975a8a29af1adf989354e1161be4
nsviel <nathan.sanchizviel@pm.me>
2024-01-23 18:44:07 +0100

    up

---
+++ src/Engine/Data/src/Namespace.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Engine/Data/src/Database/Database.h>
+#include <Engine/Data/src/Database/Scene.h>
+#include <Engine/Data/src/Database/World.h>
+#include <Engine/Data/src/Loader/Loader.h>
+#include <Engine/Data/src/Loader/Bookmark.h>
+#include <Engine/Data/src/Glyph/Glyph.h>
+#include <Engine/Data/src/Format/Format.h>
+
+
+namespace eng::scene{
+  //---------------------------
+
+  class Database;
+  class Scene;
+  class World;
+  class Bookmark;
+  class Format;
+  class Loader;
+  class Glyph;
+
+  //---------------------------
+}
