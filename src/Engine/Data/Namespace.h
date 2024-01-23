7de41030a185f963f40eddf252da9d7e7e14f035
nsviel <nathan.sanchizviel@pm.me>
2024-01-23 13:17:13 +0100

    up

--- src/Engine/Data/Namespace.h
+++ src/Engine/Data/Namespace.h
@@ -3,8 +3,8 @@
 #include <Engine/Data/Database/Database.h>
 #include <Engine/Data/Database/Scene.h>
 #include <Engine/Data/Database/World.h>
-#include <Engine/Data/Loader/Loader.h>
-#include <Engine/Data/Loader/Bookmark.h>
+#include <Engine/Data/src/Loader/Loader.h>
+#include <Engine/Data/src/Loader/Bookmark.h>
 #include <Engine/Data/Glyph/Glyph.h>
 #include <Engine/Data/Format/Format.h>
 #include <Engine/Data/Node.h>
