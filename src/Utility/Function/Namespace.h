7d755ffecaee81371ec79276056d774f2ba2901f
nsviel <nathan.sanchizviel@pm.me>
2024-01-25 15:21:49 +0100

    up

---
+++ src/Utility/Function/Namespace.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Utility/Function/File/Directory.h>
+#include <Utility/Function/File/File.h>
+#include <Utility/Function/File/Image.h>
+#include <Utility/Function/File/Info.h>
