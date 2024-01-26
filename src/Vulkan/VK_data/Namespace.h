e646ec9b354c5eb88df7a6c4649b7f63c4df1e04
nsviel <nathan.sanchizviel@pm.me>
2024-01-26 14:14:32 +0100

    up

--- src/Vulkan/VK_data/Namespace.h
+++ src/Vulkan/VK_data/Namespace.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include <Vulkan/Data/Data.h>
-#include <Vulkan/Data/Canvas.h>
-#include <Vulkan/Data/Buffer.h>
+#include <Vulkan/VK_data/Data.h>
+#include <Vulkan/VK_data/Canvas.h>
+#include <Vulkan/VK_data/Buffer.h>
