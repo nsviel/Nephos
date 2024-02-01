f2cdb2ae6afe39e555b19654a72d0a647312f333
nsviel <nathan.sanchizviel@pm.me>
2024-02-01 12:33:58 +0100

    up

--- src/Engine/Operation/gui/Namespace.h
+++ src/Engine/Operation/gui/Namespace.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include <Engine/Operation/gui/Control.h>
+#include <Engine/Operation/gui/Profiler.h>
--- src/Engine/Profiler/Namespace.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <Engine/Profiler/src/Profiler.h>
-#include <Engine/Profiler/gui/Profiler.h>
