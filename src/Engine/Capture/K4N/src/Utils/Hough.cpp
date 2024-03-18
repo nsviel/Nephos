d1e618c6172c16e5a57d64b9af1f3be31e79ab2b
nsviel <nathan.sanchizviel@pm.me>
2024-03-18 15:25:41 +0100

    up

--- src/Engine/Capture/K4N/src/Utils/Hough.cpp
+++ src/Engine/Capture/K4N/src/Utils/Hough.cpp
@@ -1,7 +1,5 @@
 #include "Hough.h"
 
-#include <opencv2/opencv.hpp>
-
 
 namespace k4n::utils{
 
@@ -9,35 +7,39 @@ namespace k4n::utils{
 Hough::Hough(){
   //---------------------------
 
+
   //---------------------------
 }
 Hough::~Hough(){}
 
-//Sphere
-void Hough::find_sphere_in_image(utl::media::Image* image){
+//Main function
+void Hough::sphere_detection(utl::media::Image* image){
   //------------------------
 
   // Create an Hough Mat object from the image data
-  cv::Mat cv_image(image->height, image->width, CV_8UC4, image->data.data());
+  cv::Mat raw_image(image->height, image->width, CV_8UC4, image->data.data());
 
-  // Convert the image to grayscale
-  cv::Mat gray_image;
-  cv::cvtColor(cv_image, gray_image, cv::COLOR_RGBA2GRAY);
+  //Pre processing
+  cv::Mat pre_image;
+  this->preprocessing(raw_image, pre_image);
+
+  //Parameter
+  this->min_dist = pre_image.rows / 8;
+  float param_1, param_2;
+  this->find_mode_parameter(param_1, param_2);
 
   // Perform Hough Transform to detect lines
   std::vector<cv::Vec3f> vec_circle;
-  int ratio = 1;
-  float min_dist = gray_image.rows / 8;
-  float min_radius = 5;
-  float max_radius = 50;
+  cv::HoughCircles(pre_image, vec_circle, mode, ratio, min_dist, param_1, param_2, min_radius, max_radius);
+  this->draw_result(pre_image, vec_circle);
 
-  //modes:
-  // - cv::HOUGH_GRADIENT
-  // - cv::HOUGH_STANDARD
-  // - cv::HOUGH_PROBABILISTIC
-  int mode = cv::HOUGH_GRADIENT;
+  //------------------------
+}
 
-  float param_1, param_2;
+//Subfunction
+void Hough::find_mode_parameter(float& param_1, float& param_2){
+  //---------------------------
+
   switch(mode){
     case cv::HOUGH_GRADIENT:{
       param_1 = 100; //higher threshold for the Canny edge detector
@@ -56,11 +58,30 @@ void Hough::find_sphere_in_image(utl::media::Image* image){
     }
   }
 
-  cv::HoughCircles(gray_image, vec_circle, mode, ratio, min_dist, param_1, param_2, min_radius, max_radius);
+  //---------------------------
+}
+void Hough::preprocessing(cv::Mat& input, cv::Mat& output){
+  //---------------------------
+
+  if(apply_canny){
+    // Perform edge detection
+    float threshold_lower = 50;
+    float threshold_upper = 150;
+    cv::Canny(input, output, threshold_lower, threshold_upper);
+  }else{
+    // Convert the image to grayscale
+    cv::Mat gray_image;
+    cv::cvtColor(input, output, cv::COLOR_RGBA2GRAY);
+  }
+
+  //---------------------------
+}
+void Hough::draw_result(cv::Mat& image, std::vector<cv::Vec3f>& vec_circle){
+  //---------------------------
 
   // Draw the detected vec_circle on the original image
   cv::Mat result;
-  cv::cvtColor(gray_image, result, cv::COLOR_RGBA2BGRA); // Convert edges to BGR for drawing
+  cv::cvtColor(image, result, cv::COLOR_RGBA2BGRA); // Convert edges to BGR for drawing
   for(size_t i=0; i<vec_circle.size(); i++){
     cv::Point center(cvRound(vec_circle[i][0]), cvRound(vec_circle[i][1]));
     int radius = cvRound(vec_circle[i][2]);
@@ -74,79 +95,10 @@ void Hough::find_sphere_in_image(utl::media::Image* image){
 
   // Display the result
   say(vec_circle.size());
-  cv::imshow("Raw Image", cv_image);
-  cv::waitKey(0);
   cv::imshow("Hough Circles", result);
   cv::waitKey(0);
 
-  //------------------------
-}
-void Hough::find_sphere_in_image_with_canny(utl::media::Image* image){
-  //------------------------
-
-  // Create an Hough Mat object from the image data
-  cv::Mat cv_image(image->height, image->width, CV_8UC4, image->data.data());
-
-  // Perform edge detection (you may need to adjust parameters for your specific image)
-  cv::Mat edges;
-  cv::Canny(cv_image, edges, 50, 150);
-
-  // Perform Hough Transform to detect lines
-  std::vector<cv::Vec3f> vec_circle;
-  int ratio = 1;
-  float min_dist = edges.rows / 8;
-  float min_radius = 5;
-  float max_radius = 50;
-
-  //modes:
-  // - cv::HOUGH_GRADIENT
-  // - cv::HOUGH_STANDARD
-  // - cv::HOUGH_PROBABILISTIC
-  int mode = cv::HOUGH_GRADIENT;
-
-  float param_1, param_2;
-  switch(mode){
-    case cv::HOUGH_GRADIENT:{
-      param_1 = 100; //higher threshold for the Canny edge detector
-      param_2 = 40; //accumulator threshold for the circle centers at the detection stage
-      break;
-    }
-    case cv::HOUGH_STANDARD:{
-      param_1 = 0; //not used
-      param_2 = 30; //minimum number of intersections to "detect" a circle
-      break;
-    }
-    case cv::HOUGH_PROBABILISTIC:{
-      param_1 = 10; //minimum distance between the centers of the detected vec_circle
-      param_2 = 30; //minimum radius of the vec_circle to detect
-      break;
-    }
-  }
-
-  cv::HoughCircles(edges, vec_circle, mode, ratio, min_dist, param_1, param_2, min_radius, max_radius);
-
-  // Draw the detected vec_circle on the original image
-  cv::Mat result;
-  cv::cvtColor(edges, result, cv::COLOR_RGBA2BGRA); // Convert edges to BGR for drawing
-  for(size_t i=0; i<vec_circle.size(); i++){
-    cv::Point center(cvRound(vec_circle[i][0]), cvRound(vec_circle[i][1]));
-    int radius = cvRound(vec_circle[i][2]);
-
-    // Draw the circle center
-    cv::circle(result, center, 3, cv::Scalar(0, 255, 0), -1, cv::LINE_AA);
-
-    // Draw the circle outline
-    cv::circle(result, center, radius, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
-  }
-
-  // Display the result
-  say(vec_circle.size());
-  cv::imshow("Raw Image", cv_image);
-  cv::waitKey(0);
-  cv::imshow("Hough Circles", result);
-  cv::waitKey(0);
-
-  //------------------------
+  //---------------------------
 }
 
 }
--- src/Engine/Capture/K4N/src/Utils/Hough.h
+++ src/Engine/Capture/K4N/src/Utils/Hough.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Utility/Specific/common.h>
+#include <opencv2/opencv.hpp>
 
 
 namespace k4n::hough{
@@ -22,12 +23,28 @@ public:
   ~Hough();
 
 public:
-  //Sphere fitting
-  void find_sphere_in_image(utl::media::Image* image);
-  void find_sphere_in_image_with_canny(utl::media::Image* image);
+  //Main function
+  void sphere_detection(utl::media::Image* image);
+
+  //Subfunction
+  void find_mode_parameter(float& param_1, float& param_2);
+  void preprocessing(cv::Mat& input, cv::Mat& output);
+  void draw_result(cv::Mat& image, std::vector<cv::Vec3f>& vec_circle);
+
+  inline int* get_mode(){return &mode;}
+  inline int* get_ratio(){return &ratio;}
+  inline float* get_min_dist(){return &min_dist;}
+  inline float* get_min_radius(){return &min_radius;}
+  inline float* get_max_radius(){return &max_radius;}
+  inline bool* get_apply_canny(){return &apply_canny;}
 
 private:
   int mode = k4n::hough::GRADIENT;
+  int ratio = 1;
+  float min_dist = 0;
+  float min_radius = 5;
+  float max_radius = 50;
+  bool apply_canny = false;
 };
 
 }
