a8a3aa7510cdd77447fb5e8c0077c4a52ccc4157
nsviel <nathan.sanchizviel@pm.me>
2024-04-04 13:41:30 +0200

    up_pcap

--- src/Engine/Capture/Velodyne/src/Structure/Namespace.h
+++ src/Engine/Capture/Velodyne/src/Structure/Namespace.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include <Velodyne/src/Structure/Player.h>
+#include <Velodyne/src/Structure/Velodyne.h>
--- src/Engine/Capture/Velodyne/src/Structure/Velodyne.h
+++ src/Engine/Capture/Velodyne/src/Structure/Velodyne.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Velodyne/src/Structure/Player.h>
+
 
 namespace velodyne::structure{
 
