df6ca2c85f935482d60de71d40fd9a23fbce4741
nsviel <nathan.sanchizviel@pm.me>
2024-04-10 17:42:45 +0200

    up

--- src/Vulkan/Node.cpp
+++ src/Vulkan/Node.cpp
@@ -10,16 +10,15 @@ namespace vk{
 
 //Constructor / Destructor
 Node::Node(app::Node* node_app){
+
   //---------------------------
 
   prf::Node* node_profiler = node_app->get_node_profiler();
 
   this->vk_struct = new vk::structure::Vulkan(node_profiler);
-
   vk_struct->param.headless = false;
-  if(vk_struct->param.headless) this->vk_engine = new vk::main::Headless(vk_struct);
-  else this->vk_engine = new vk::main::Graphical(vk_struct);
 
+  this->vk_engine = vk_struct->param.headless ? static_cast<vk::main::Engine*>(new vk::main::Graphical(vk_struct)) : static_cast<vk::main::Engine*>(new vk::main::Graphical(vk_struct));
   this->vk_render = new vk::render::Manager(vk_struct);
   this->vk_imgui = new vk::gui::Imgui(vk_struct);
   this->vk_interface = new vk::main::Interface(vk_struct);
