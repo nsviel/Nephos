96ffd6e9388714f93a85b34ece97a5439455e9a7
nsviel <nathan.sanchizviel@pm.me>
2024-04-10 16:15:36 +0200

    up

--- src/Engine/Render/Namespace.h
+++ src/Engine/Render/Namespace.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Render/Node.h>
+#include <Render/Manager.h>
 #include <Render/EDL/Namespace.h>
 #include <Render/GUI/Namespace.h>
 #include <Render/Scene/Namespace.h>
-#include <Render/Renderpass/Namespace.h>
