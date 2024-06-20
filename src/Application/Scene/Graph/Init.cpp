9ed41942e076c65f6e8cb96923fbb9588dba5918
nsviel <nathan.sanchizviel@pm.me>
2024-06-20 20:49:17 +0200

    up_path_struct

--- src/Application/Scene/Graph/Init.cpp
+++ src/Application/Scene/Graph/Init.cpp
@@ -35,36 +35,30 @@ void Init::load_entity(){
 
   utl::base::Path versaille_0("/home/aether/Desktop/versaille_0.mkv");
   utl::base::Path versaille_0_t("/home/aether/Desktop/versaille_0.json");
-/*
-  utl::base::Path versaille_2;
-  versaille_2.data = "/home/aether/Desktop/versaille_2.mkv";
-  versaille_2.transformation = "/home/aether/Desktop/versaille_2.json";
 
-  utl::base::Path cerfav;
-  cerfav.data = "/home/aether/Desktop/cerfav_3_1.mkv";
+  utl::base::Path versaille_2("/home/aether/Desktop/versaille_2.mkv");
+  utl::base::Path versaille_2_t("/home/aether/Desktop/versaille_2.json");
 
-  utl::base::Path dragon;
-  dragon.data = "/home/aether/Desktop/Nephos/media/point_cloud/dragon.ply";
+  utl::base::Path cerfav("/home/aether/Desktop/cerfav_3_1.mkv");
 
-  utl::base::Path sphere;
-  sphere.data = "/home/aether/Desktop/Sphere_good.mkv";
+  utl::base::Path dragon("/home/aether/Desktop/Nephos/media/point_cloud/dragon.ply");
+
+  utl::base::Path sphere("/home/aether/Desktop/Sphere_good.mkv");
   //sphere.data = "/home/aether/Desktop/bench_1.mkv";
 
-  utl::base::Path pcap;
-  pcap.data = "/home/aether/Desktop/pcap_test.pcap";
+  utl::base::Path pcap("/home/aether/Desktop/pcap_test.pcap");
 
-  utl::base::Path mire;
-  mire.data = "/home/aether/Desktop/Mire.mkv";
-*/
+  utl::base::Path mire("/home/aether/Desktop/Mire.mkv");
+
   //Create playback list
-  ldr_loader->load_object(versaille_0, versaille_0_t);
-  //ldr_loader->load_object(versaille_2);
+  //ldr_loader->load_object(versaille_0, versaille_0_t);
+  //ldr_loader->load_object(versaille_2, versaille_2_t);
   //ldr_loader->load_object(cerfav);
   //ldr_loader->load_object(dragon);
   //ldr_loader->load_object(sphere);
   //ldr_loader->load_set(pcap);
   //ldr_loader->load_object(mire);
-  //ldr_loader->load_object(sphere);
+  ldr_loader->load_object(sphere);
 
   //----------------------------
 }
