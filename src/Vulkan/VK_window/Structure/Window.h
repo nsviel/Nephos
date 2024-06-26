e05373392a02e74597ef13fc1c16469ee12bb37a
nsviel <nathan.sanchizviel@pm.me>
2024-06-26 21:33:01 +0200

    up

--- src/Vulkan/VK_window/Structure/Window.h
+++ src/Vulkan/VK_window/Structure/Window.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include <Utility/Specific/Common.h>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 
 namespace vk::structure{
@@ -14,14 +18,14 @@ struct Window{
   bool headless = false;
 
   //Dimension
-  bool resizing = false;
-  bool* running = nullptr;
+  VkExtent2D extent = {};
+  VkSurfaceKHR surface = VK_NULL_HANDLE;
   glm::vec2 dimension =  glm::vec2(1024, 500);
   glm::vec2 constraint_min = glm::vec2(500, 250);
   glm::vec2 constraint_max = glm::vec2(4000, 4000);
   glm::vec2 center = glm::vec2(0, 0);
-  VkExtent2D extent = {};
-  VkSurfaceKHR surface = VK_NULL_HANDLE;
+  bool resizing = false;
+  bool* running = nullptr;
   int cpt = 0;
 
   //---------------------------
