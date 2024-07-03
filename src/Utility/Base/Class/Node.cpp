ed89f9df9bf43435ddf0beb221275eb3313861e7
nsviel <nathan.sanchizviel@pm.me>
2024-07-03 16:37:59 +0200

    up

--- src/Utility/Base/Class/Node.cpp
+++ src/Utility/Base/Class/Node.cpp
@@ -12,6 +12,23 @@ Node::Node(){
 }
 Node::~Node(){}
 
-//Main function
+//Subfunction
+utl::gui::Panel* Node::add_panel(std::string name, std::string icon, bool value){
+  //---------------------------
+
+  utl::gui::Panel* panel = new utl::gui::Panel(name, icon, value);
+  this->vec_panel.push_back(panel);
+
+  //---------------------------
+  return panel;
+}
+void Node::add_node_panel(utl::base::Node* node){
+  //---------------------------
+
+  std::vector<utl::gui::Panel*> node_vec_panel = node->get_vec_panel();
+  this->vec_panel.insert(vec_panel.end(), node_vec_panel.begin(), node_vec_panel.end());
+
+  //---------------------------
+}
 
 }
--- src/Utility/Base/Class/Node.h
+++ src/Utility/Base/Class/Node.h
@@ -23,23 +23,9 @@ public:
   virtual void wait(){}
   virtual void config(){}
 
-  utl::gui::Panel* add_panel(std::string name, std::string icon, bool value){
-    //---------------------------
-
-    utl::gui::Panel* panel = new utl::gui::Panel(name, icon, value);
-    this->vec_panel.push_back(panel);
-
-    //---------------------------
-    return panel;
-  }
-  void add_node_panel(utl::base::Node* node){
-    //---------------------------
-
-    std::vector<utl::gui::Panel*> node_vec_panel = node->get_vec_panel();
-    this->vec_panel.insert(vec_panel.end(), node_vec_panel.begin(), node_vec_panel.end());
-
-    //---------------------------
-  }
+  //Subfunction
+  utl::gui::Panel* add_panel(std::string name, std::string icon, bool value);
+  void add_node_panel(utl::base::Node* node);
 
   inline std::vector<utl::gui::Panel*> get_vec_panel(){return vec_panel;}
 
