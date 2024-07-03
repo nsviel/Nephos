a4602c3092d3454cbf0ca793b0f26380b34d9c99
nsviel <nathan.sanchizviel@pm.me>
2024-07-03 18:55:54 +0200

    up

--- src/Engine/Interface/gui/State/Namespace.h
+++ src/Engine/Interface/gui/State/Namespace.h
@@ -1,7 +1,3 @@
 #pragma once
 
 #include <Interface/gui/State/Manager.h>
-#include <Interface/gui/State/Saver.h>
-#include <Interface/gui/State/Loader.h>
-#include <Interface/gui/State/Logic.h>
-#include <Interface/gui/State/Structure.h>
---
+++ src/Engine/Interface/src/State/Namespace.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Interface/src/State/Saver.h>
+#include <Interface/src/State/Loader.h>
+#include <Interface/src/State/Logic.h>
+#include <Interface/src/State/Structure.h>
