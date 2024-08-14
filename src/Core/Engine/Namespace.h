c436d268129fced9636c7c009f511416bbc4ea98
nsviel <nathan.sanchizviel@pm.me>
2024-08-14 21:37:30 +0200

    up

---
+++ src/Core/Engine/Namespace.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Engine/Node.h>
+#include <Engine/Camera/Namespace.h>
+#include <Engine/Control/Namespace.h>
+#include <Engine/Renderer/Namespace.h>
--- src/Core/Engine/Node.cpp
+++ src/Core/Engine/Node.cpp
@@ -1,33 +1,16 @@
 #include "Node.h"
-
-#include <Application/Node.h>
-#include <Vulkan/Namespace.h>
-#include <Core/Namespace.h>
-#include <Utility/Namespace.h>
-#include <Profiler/Namespace.h>
-#include <Camera/Namespace.h>
-#include <Renderer/Namespace.h>
-#include <IO/Namespace.h>
-#include <Dynamic/Namespace.h>
-#include <Radiometry/Namespace.h>
-#include <Operation/Namespace.h>
-#include <Control/Namespace.h>
-#include <Data/Namespace.h>
+#include "Namespace.h"
 
 
-namespace eng{
+namespace eng_{
 
 //Constructor / Destructor
-Node::Node(app::Node* node_app){
+Node::Node(eng::Node* node_engine){
   //---------------------------
 
-  this->node_camera = new cam::Node(this);
-  this->node_control = new ctl::Node(this);
-  this->node_interface = new rnd::Node(this);
-
-  //Tasker CPU
-  prf::dynamic::Manager* prf_dynamic = node_profiler->get_prf_dynamic();
-  this->tasker = prf_dynamic->get_tasker_cpu();
+  this->node_camera = new cam::Node(node_engine);
+  this->node_control = new ctl::Node(node_engine);
+  this->node_renderer = new rnd::Node(node_engine);
 
   //---------------------------
 }
@@ -36,55 +19,23 @@ Node::~Node(){}
 void Node::init(){
   //---------------------------
 
-  node_data->init();
-  node_io->init();
   node_camera->init();
-  node_interface->init();
-  node_radio->init();
-  node_profiler->init();
+  node_renderer->init();
 
   //---------------------------
 }
 void Node::loop(){
   //---------------------------
 
-  node_profiler->loop();
-  tasker->task_begin("eng::loop");
-  node_data->loop();
   node_camera->loop();
-  node_dynamic->loop();
-  node_radio->loop();
-  tasker->task_end("eng::loop");
-
-  tasker->task_begin("eng::vulkan");
-  node_vulkan->loop();
-  tasker->task_end("eng::vulkan");
 
   //---------------------------
 }
 void Node::gui(){
   //---------------------------
 
-  tasker->task_begin("eng::gui");
-
   node_camera->gui();
-  node_data->gui();
-  node_io->gui();
-  node_interface->gui();
-  node_dynamic->gui();
-  node_radio->gui();
-
-  tasker->task_end("eng::gui");
-  node_profiler->gui();
-
-  //---------------------------
-}
-void Node::clean(){
-  //---------------------------
-
-  node_data->clean();
-  node_io->clean();
-  node_vulkan->clean();
+  node_renderer->gui();
 
   //---------------------------
 }
@@ -92,15 +43,6 @@ void Node::reset(){
   //---------------------------
 
   node_camera->reset();
-  node_data->reset();
-  node_dynamic->reset();
-
-  //---------------------------
-}
-void Node::close(){
-  //---------------------------
-
-  node_app->close();
 
   //---------------------------
 }
