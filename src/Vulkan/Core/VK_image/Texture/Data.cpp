1448e6c2c44486c6c6102733889c926fc65bebd5
nsviel <nathan.sanchizviel@pm.me>
2024-09-23 18:58:22 +0200

    reword_image_texture_not_totally_good

--- src/Vulkan/Core/VK_image/Texture/Data.cpp
+++ src/Vulkan/Core/VK_image/Texture/Data.cpp
@@ -20,23 +20,31 @@ Data::~Data(){}
 
 //Main function
 void Data::insert_texture(utl::base::Data& data, std::shared_ptr<utl::media::Image> image){say("---");
-  if(!check_image(image)) return;
   //---------------------------
 
   //Retrieve data vk object
   auto vk_object = vk_data->retrieve_vk_object(data);
   if(!vk_object) return;
 
+  //Insert in vk object
+  this->insert_texture(*vk_object, image);
+
+  //---------------------------
+}
+void Data::insert_texture(vk::structure::Object& vk_object, std::shared_ptr<utl::media::Image> image){
+  if(!check_image(image)) return;
+  //---------------------------
+
   //Check if image already inserted
-  auto vk_texture = vk_data->retrieve_vk_texture(*vk_object, image->name);
+  auto vk_texture = vk_data->retrieve_vk_texture(vk_object, image->name);
   if(vk_texture) return;
-sayHello();
+
   //Create texture from image and insert
   auto texture = std::make_shared<vk::structure::Texture>();
   texture->image = image;
   vk_ressource->create_texture(*texture);
-  vk_object->map_texture[image->name] = texture;
-say(vk_object->map_texture.size());
+  vk_object.map_texture[image->name] = texture;
+
   //---------------------------
 }
 void Data::insert_texture(std::shared_ptr<utl::media::Image> image){
@@ -71,15 +79,6 @@ void Data::update_texture(std::shared_ptr<utl::media::Image> image){
 }
 
 //Subfunction
-void Data::create_texture(vk::structure::Object& vk_object, std::shared_ptr<utl::media::Image> image){
-  //---------------------------
-
-  auto texture = std::make_shared<vk::structure::Texture>();
-  texture->image = image;
-  vk_ressource->create_texture(*texture);
-
-  //---------------------------
-}
 void Data::clean_texture(vk::structure::Object& vk_object){
   //---------------------------
 
