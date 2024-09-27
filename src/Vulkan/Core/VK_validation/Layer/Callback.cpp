25efc426b32dbbe542ccd9192ad9e846b73992d4
nsviel <nathan.sanchizviel@pm.me>
2024-09-27 17:12:52 +0200

    up

---
+++ src/Vulkan/Core/VK_validation/Layer/Callback.cpp
@@ -0,0 +1,29 @@
+#include "Callback.h"
+
+#include <Vulkan/Core/VK_validation/Log/Debug.h>
+#include <Vulkan/Core/VK_validation/Log/Shader.h>
+#include <cstring>
+
+
+namespace vk::validation{
+
+//Validation layer callback function
+VKAPI_ATTR VkBool32 VKAPI_CALL Callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, VkDebugUtilsMessageTypeFlagsEXT message_type, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData){
+  std::string message(pCallbackData->pMessage);
+  //---------------------------
+
+  //Shader printf
+  size_t shaderPos = message.find("[SHADER]");
+  if(shaderPos != std::string::npos){
+    vk::validation::log::Shader::get_instance().add_shader_printf(message);
+  }
+  //Common validation layer message
+  else if(message_type == VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT){
+    vk::validation::log::Debug::get_instance().print_validation_error(message);
+  }
+
+  //---------------------------
+  return VK_FALSE;
+}
+
+}
--- src/Vulkan/Core/VK_validation/Layer/Callback.h
+++ src/Vulkan/Core/VK_validation/Layer/Callback.h
@@ -1,29 +1,12 @@
 #pragma once
 
-#include <Vulkan/Core/VK_validation/Log/Debug.h>
-#include <Vulkan/Core/VK_validation/Log/Shader.h>
+#include <vulkan/vulkan.h>
 #include <cstring>
 
 
 namespace vk::validation{
 
 //Validation layer callback function
-static VKAPI_ATTR VkBool32 VKAPI_CALL Callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,VkDebugUtilsMessageTypeFlagsEXT message_type,const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,void* pUserData){
-  std::string message(pCallbackData->pMessage);
-  //---------------------------
-
-  //Shader printf
-  size_t shaderPos = message.find("[SHADER]");
-  if(shaderPos != std::string::npos){
-    vk::validation::log::Shader::get_instance().add_shader_printf(message);
-  }
-  //Common validation layer message
-  else if(message_type == VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT){
-    vk::validation::log::Debug::get_instance().print_validation_error(message);
-  }
-
-  //---------------------------
-  return VK_FALSE;
-}
+VKAPI_ATTR VkBool32 VKAPI_CALL Callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, VkDebugUtilsMessageTypeFlagsEXT message_type, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
 
 }
