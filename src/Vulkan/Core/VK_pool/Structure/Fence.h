39cf00484b2a35cf89528ab5f410d79fa35c4cf5
nsviel <nathan.sanchizviel@pm.me>
2024-10-02 17:28:55 +0200

    up

--- src/Vulkan/Core/VK_pool/Structure/Fence.h
+++ src/Vulkan/Core/VK_pool/Structure/Fence.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <Vulkan/Core/VK_command/Structure/Command_buffer.h>
 #include <Vulkan/Core/VK_synchronization/Structure/Fence.h>
-#include <Vulkan/Core/VK_synchronization/Structure/Semaphore.h>
-#include <Vulkan/Core/VK_binding/Structure/Descriptor.h>
 #include <vulkan/vulkan.h>
 #include <vector>
 #include <mutex>
