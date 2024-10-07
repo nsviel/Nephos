3717a40ba3d25a6aafc2ec0ffc2b7154e4396a86
nsviel <nathan.sanchizviel@pm.me>
2024-10-07 18:00:49 +0200

    up

--- shader/GS/Render/glsl/common.glsl
+++ shader/GS/Render/glsl/common.glsl
@@ -11,8 +11,6 @@
 #define assert( condition, message, value )
 #endif
 
-#define MAGIC 0x4d415449u
-
 const float SH_C0 = 0.28209479177387814f;
 const float SH_C1 = 0.4886025119029199f;
 const float SH_C2[] = {
@@ -33,43 +31,42 @@ const float SH_C3[] = {
 };
 
 struct Vertex {
-    vec4 position;
-    vec4 scale_opacity;
-    vec4 rotation;
-    float sh[48];
+  vec4 position;
+  vec4 scale_opacity;
+  vec4 rotation;
+  float sh[48];
 };
 
-struct VertexAttribute {
-    vec4 conic_opacity;
-    vec4 color_radii;
-    uvec4 aabb;
-    vec2 uv;
-    float depth;
-    uint magic;
+struct VertexAttribute{
+  vec4 conic_opacity;
+  vec4 color_radii;
+  uvec4 aabb;
+  vec2 uv;
+  float depth;
 };
 
 mat3 rotationFromQuaternion(vec4 q){
-    float qx = q.y;
-    float qy = q.z;
-    float qz = q.w;
-    float qw = q.x;
+  float qx = q.y;
+  float qy = q.z;
+  float qz = q.w;
+  float qw = q.x;
 
-    float qx2 = qx * qx;
-    float qy2 = qy * qy;
-    float qz2 = qz * qz;
+  float qx2 = qx * qx;
+  float qy2 = qy * qy;
+  float qz2 = qz * qz;
 
-    mat3 rotationMatrix;
-    rotationMatrix[0][0] = 1 - 2 * qy2 - 2 * qz2;
-    rotationMatrix[0][1] = 2 * qx * qy - 2 * qz * qw;
-    rotationMatrix[0][2] = 2 * qx * qz + 2 * qy * qw;
+  mat3 rotationMatrix;
+  rotationMatrix[0][0] = 1 - 2 * qy2 - 2 * qz2;
+  rotationMatrix[0][1] = 2 * qx * qy - 2 * qz * qw;
+  rotationMatrix[0][2] = 2 * qx * qz + 2 * qy * qw;
 
-    rotationMatrix[1][0] = 2 * qx * qy + 2 * qz * qw;
-    rotationMatrix[1][1] = 1 - 2 * qx2 - 2 * qz2;
-    rotationMatrix[1][2] = 2 * qy * qz - 2 * qx * qw;
+  rotationMatrix[1][0] = 2 * qx * qy + 2 * qz * qw;
+  rotationMatrix[1][1] = 1 - 2 * qx2 - 2 * qz2;
+  rotationMatrix[1][2] = 2 * qy * qz - 2 * qx * qw;
 
-    rotationMatrix[2][0] = 2 * qx * qz - 2 * qy * qw;
-    rotationMatrix[2][1] = 2 * qy * qz + 2 * qx * qw;
-    rotationMatrix[2][2] = 1 - 2 * qx2 - 2 * qy2;
+  rotationMatrix[2][0] = 2 * qx * qz - 2 * qy * qw;
+  rotationMatrix[2][1] = 2 * qy * qz + 2 * qx * qw;
+  rotationMatrix[2][2] = 1 - 2 * qx2 - 2 * qy2;
 
-    return rotationMatrix;
+  return rotationMatrix;
 }
