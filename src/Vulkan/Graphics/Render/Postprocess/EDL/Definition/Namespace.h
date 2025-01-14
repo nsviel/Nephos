5f653c85bdd27c39c126bef08cce33ba6922198d
nsviel <nathan.sanchizviel@pm.me>
2025-01-14 12:41:56 +0100

    up

--- src/Vulkan/Graphics/Render/Postprocess/EDL/Definition/Namespace.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Vulkan/Graphics/Render/Postprocess/EDL/Definition/Descriptor.h>
-#include <Vulkan/Graphics/Render/Postprocess/EDL/Definition/Pipeline.h>
-#include <Vulkan/Graphics/Render/Postprocess/EDL/Definition/Structure.h>
--- src/Vulkan/Graphics/Render/Postprocess/EDL/Namespace.h
+++ src/Vulkan/Graphics/Render/Postprocess/EDL/Namespace.h
@@ -1,4 +1,6 @@
 #pragma once
 
-#include <Vulkan/Graphics/Render/Postprocess/EDL/Definition/Namespace.h>
+#include <Vulkan/Graphics/Render/Postprocess/EDL/Descriptor.h>
+#include <Vulkan/Graphics/Render/Postprocess/EDL/Pipeline.h>
+#include <Vulkan/Graphics/Render/Postprocess/EDL/Structure.h>
 #include <Vulkan/Graphics/Render/Postprocess/EDL/Subpass.h>
