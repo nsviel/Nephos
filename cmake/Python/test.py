3092dbe7c2e6f795528c94c2c937e204a0100fc2
nsviel <nathan.sanchizviel@pm.me>
2025-01-21 15:21:31 +0100

    up

--- cmake/Python/test.py
+++ cmake/Python/test.py
@@ -5,21 +5,23 @@ from pynput import keyboard
 
 #Nephos stuff
 nephos = pynephos.App()
-nephos.run()
-nephos.add_cloud()
 
-#Main loop
-def on_press(key):
-    try:
-        if key == keyboard.Key.esc:
-            print("Esc key pressed. Exiting loop...")
-            return False  # This will stop the listener
-    except AttributeError:
-        pass
+if 0:
+    nephos.run()
+    nephos.add_cloud()
 
-# Start listening for keyboard input
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+    #Main loop
+    def on_press(key):
+        try:
+            if key == keyboard.Key.esc:
+                print("Esc key pressed. Exiting loop...")
+                return False  # This will stop the listener
+        except AttributeError:
+            pass
 
-#Nephos close
-nephos.close()
+    # Start listening for keyboard input
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+    #Nephos close
+    nephos.close()
